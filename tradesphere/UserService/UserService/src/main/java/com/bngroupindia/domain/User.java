955a622ee83a686ff17c05a64e440e20c7de7a15
mohini <it.intern@bngroupindia.com>
2026-06-11 14:55:16 +0530

    added some entities in the user class

---
+++ tradesphere/UserService/UserService/src/main/java/com/bngroupindia/domain/User.java
@@ -0,0 +1,14 @@
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructo
+public class User {
+   private int id;
+   private String name;
+   private String role;
+    private long phoneNumber;
+    @id
+    private string email;
+    private string password;
+
+}
