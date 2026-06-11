fe44e16b5641b7adb5919914518b9444c1d2a29d
mohini <it.intern@bngroupindia.com>
2026-06-11 11:43:31 +0530

    added some services

---
+++ tradesphere/promoter/promoter/src/test/java/com/bngroup/promoter/PromoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bngroup.promoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ tradesphere/warehouses/warehouses/src/test/java/com/bngroup/warehouses/WarehousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bngroup.warehouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
