9917e54ed1643fa8ab8ad9cae9c7b898fecda9f2
mohini <it.intern@bngroupindia.com>
2026-06-11 17:16:40 +0530

    added domain folder in each services

---
+++ tradesphere/EmployeeService/EmployeeService/src/main/java/com/bngroupindia/domain/employee.java
@@ -0,0 +1,15 @@
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class employee {
+    @id
+    private string empId;
+    private string empName;
+    private string designation;
+    private string reportingManager;
+
+
+    pri
+}
