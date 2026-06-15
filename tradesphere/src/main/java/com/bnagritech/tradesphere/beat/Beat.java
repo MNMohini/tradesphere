c37301223036376de63a5e1c640f5baa49a13ac3
mohini <it.intern@bngroupindia.com>
2026-06-15 14:49:16 +0530

    created Beat.java class and added some attributes in the class

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/Beat.java
@@ -0,0 +1,22 @@
+package com.bnagritech.tradesphere.beat;
+
+import jakarta.validation.constraints.Max;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Beat {
+    private String beatId;
+    private String beatCode;
+    private String beatName;
+    private String territoryId;
+    private String assignedEmployeeID;
+    private String description;
+    private List<String> visitDay;
+    private Integer retailerCount;
+    private Boolean active;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private String updatedBy;
+}
