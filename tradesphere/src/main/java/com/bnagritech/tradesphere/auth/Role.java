e3c9d07ef5e0ac63d54d753fc7717b3a4bf9cc5d
mohini <it.intern@bngroupindia.com>
2026-06-15 12:22:11 +0530

    in the Role class added more attributes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/Role.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/Role.java
@@ -1,8 +1,15 @@
 package com.bnagritech.tradesphere.auth;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Role {
     private int roleId;
     private String roleName;
-    private int permissionId;
-
+    private String roleCode;
+    private String description;
+    private List<String> permissionIds;
+    private boolean active;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
 }
