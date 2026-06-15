cd6d04f50f01ab521dc03fb3950d907d833531f9
mohini <it.intern@bngroupindia.com>
2026-06-15 12:42:50 +0530

    in the Employee.java class added some attributes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/Employee.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/Employee.java
@@ -1,4 +1,18 @@
 package com.bnagritech.tradesphere.employee;
 
+import java.time.LocalDateTime;
+
 public class Employee {
+    private String employeeId;
+    private String employeeName;
+    private String email;
+    private String userName;
+    private String managerId;
+    private long MobileNumber;
+    private String territoryId;
+    private LocalDateTime joiningDate;
+    private Boolean status;
+
+
+
 }
