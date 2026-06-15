7904c64e875b1a193857a6c8efd0056027bedd66
mohini <it.intern@bngroupindia.com>
2026-06-15 13:15:43 +0530

    Created TerritoryType.java class in territory package and Added some attributes in Territory.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/territory/Territory.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/Territory.java
@@ -1,4 +1,23 @@
 package com.bnagritech.tradesphere.territory;
 
+import java.time.LocalDateTime;
+
 public class Territory {
+    private String territoryId;
+    private String territoryCode;
+    private String territoryName;
+    private TerritoryType territoryType;
+    private String parentTerritoryId;
+    private String description;
+    private String managerId;
+    private Boolean active;
+
+    private LocalDateTime updateAt;
+    private String createdBy;
+    private String updatedBy;
+
+
+
+
+
 }
