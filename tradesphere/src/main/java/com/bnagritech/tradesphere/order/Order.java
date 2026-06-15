052dfbbba2e53defb0627fd1f1c7771b66664315
mohini <it.intern@bngroupindia.com>
2026-06-15 18:13:59 +0530

    created Order.java class

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/Order.java
@@ -0,0 +1,10 @@
+package com.bnagritech.tradesphere.order;
+
+import java.time.LocalDateTime;
+
+public class Order {
+    private int id;
+    private String orderId;
+    private String retailerId;
+    private String employeeId;
+}
