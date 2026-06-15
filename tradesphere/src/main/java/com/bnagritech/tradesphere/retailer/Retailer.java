0660793509947b7851bdbfb1a95703ffbdafb043
mohini <it.intern@bngroupindia.com>
2026-06-15 14:18:56 +0530

    in the Retailer.java class added some required attributes

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/retailer/Retailer.java
@@ -0,0 +1,33 @@
+package com.bnagritech.tradesphere.retailer;
+
+import java.time.LocalDateTime;
+
+public class Retailer {
+    private String retailerId;
+    private String  retailerCode;
+    private String shopName;
+    private String ownerName;
+    private String contactNumber;
+    private String alternateContactNumber;
+    private String retailerEmail;
+    private String address;
+    private String territoryId;
+    private String beatId;
+    private String promoterId;
+    private RetailerType retailerType;
+    private String gstNumber;
+    private String panNumber;
+    private Double latitude;
+    private Double longitude;
+    private Double creditDays;
+    private Double creditLimits;
+    private RetailerStatus retailerStatus;
+    private boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private  String updatedBy;
+
+
+
+}
