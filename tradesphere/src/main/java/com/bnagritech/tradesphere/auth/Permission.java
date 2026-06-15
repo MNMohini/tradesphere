9c55d9498cc414c020cb3f9998d661a018cc976a
mohini <it.intern@bngroupindia.com>
2026-06-15 12:27:49 +0530

    in the Permission class added some attributes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/Permission.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/Permission.java
@@ -1,4 +1,15 @@
 package com.bnagritech.tradesphere.auth;
 
+import java.time.LocalDateTime;
+
 public class Permission {
+    private String permissionId;
+    private String permissionCode;
+    private String permissionName;
+    private String module;
+    private String action;
+    private String description;
+    private Boolean active;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
 }
