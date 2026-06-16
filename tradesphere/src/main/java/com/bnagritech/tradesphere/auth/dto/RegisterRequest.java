fd20a7c4943290b32653c7bac8f1afee04deccd5
mohini <it.intern@bngroupindia.com>
2026-06-16 14:11:52 +0530

    created RegisterRequest.javaclass and added sme attribute

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package com.bnagritech.tradesphere.auth.dto;
+
+import com.bnagritech.tradesphere.common.UserRole;
+
+public class RegisterRequest {
+    private  String userName;
+    private String password;
+    private String employeeId;
+    private UserRole role;
+}
