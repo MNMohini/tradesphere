e32e72c2c5987bfa5b0d9bbe4a27d15450c76d1c
mohini <it.intern@bngroupindia.com>
2026-06-16 14:30:49 +0530

    added some annotation

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterRequest.java
@@ -1,10 +1,19 @@
 package com.bnagritech.tradesphere.auth.dto;
 
 import com.bnagritech.tradesphere.common.UserRole;
+import jakarta.validation.constraints.NotBlank;
 
 public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
     private  String userName;
+
+    @NotBlank(message = "password is required")
     private String password;
+
+    @NotBlank(message = "Employee is required")
     private String employeeId;
+
+    @NotBlank(message = "Role is required")
     private UserRole role;
 }
