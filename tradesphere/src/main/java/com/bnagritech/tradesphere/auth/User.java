87e672639b0c69e39c3b33b8a34f9961c4d4663c
mohini <it.intern@bngroupindia.com>
2026-06-16 14:08:06 +0530

    User.java class connected with User collection and created LoginResponse.java and add some attributes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/User.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/User.java
@@ -1,10 +1,18 @@
 package com.bnagritech.tradesphere.auth;
 
+import com.bnagritech.tradesphere.common.UserRole;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "User")
 public class User {
+    @Id
     private String id;
     private String userName;
     private String password;
     private String employeeId;
-    private String roleId;
+    private UserRole role;
+    private Boolean active;
+
 
 }
---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.bnagritech.tradesphere.auth.dto;
+
+public class LoginResponse {
+    private String accessToken;
+    private String userName;
+    private String role;
+
+}
