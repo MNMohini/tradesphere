fbacf767cfcc59c0c2f36e25556ebf43a629d2ce
mohini <it.intern@bngroupindia.com>
2026-06-16 13:24:27 +0530

    Created UserRole.java class and add some enums

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/UserRole.java
@@ -0,0 +1,11 @@
+package com.bnagritech.tradesphere.common;
+
+public enum UserRole {
+    ADMIN,
+    NATIONAL_HEAD,
+    REGIONAL_MANAGER,
+    STATE_MANAGER,
+    AREA_MANAGER,
+    DISTRICT_MANAGER,
+    PROMOTER
+}
