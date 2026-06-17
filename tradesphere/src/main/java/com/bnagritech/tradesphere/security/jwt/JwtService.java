cdc42feb8c148d879fff96638606a03c2abba404
mohini <it.intern@bngroupindia.com>
2026-06-17 13:24:09 +0530

    update JwtService.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/security/jwt/JwtService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/security/jwt/JwtService.java
@@ -1,34 +1,36 @@
 package com.bnagritech.tradesphere.security.jwt;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
-
     @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.expiration}")
     private long expiration;
 
-    private  String generateToken(String userName){
-        return OAuth2ResourceServerProperties.Jwt.builder()
-                .subject(userName)
+    public String generateToken(String username) {
+
+        return Jwts.builder()
+                .subject(username)
                 .issuedAt(new Date())
                 .expiration(
                         new Date(
                                 System.currentTimeMillis() + expiration
                         )
-                        )
+                )
                 .signWith(
                         io.jsonwebtoken.security.Keys.hmacShaKeyFor(
                                 secret.getBytes()
                         )
-                ).compact();
-
+                )
+                .compact();
     }
+
+
 }
