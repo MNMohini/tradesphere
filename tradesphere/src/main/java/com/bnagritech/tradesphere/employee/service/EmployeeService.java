ed4ab73b131eb0071969677ad34cfd808e8ca4f1
mohini <it.intern@bngroupindia.com>
2026-06-18 14:56:25 +0530

    EmployeeService.java interface created and create some abstract methods

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.bnagritech.tradesphere.employee.service;
+
+import com.bnagritech.tradesphere.employee.dto.EmployeeRequest;
+import com.bnagritech.tradesphere.employee.dto.EmployeeResponse;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeResponse createEmployee(EmployeeRequest request);
+    List<EmployeeResponse> getAllEmployees();
+    EmployeeResponse getEmployeeByUserName(String userName);
+    EmployeeResponse UpdateEmployee(String employeeId, EmployeeRequest request);
+    void  deleteEmployee(String employeeId);
+}
