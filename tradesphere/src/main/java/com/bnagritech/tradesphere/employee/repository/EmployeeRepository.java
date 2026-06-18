faf64233486b2d240498b1951356c0d8aec8f529
mohini <it.intern@bngroupindia.com>
2026-06-18 14:44:27 +0530

    EmployeeRepository.java interface created and create some abstract methods

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.bnagritech.tradesphere.employee.repository;
+import com.bnagritech.tradesphere.employee.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+
+    Optional<Employee> findByEmail(String email);
+    Optional<Employee> findByUserName(String userName);
+    Optional<Employee> findByEmployeeId(String employeeId);
+
+     boolean existsByEmail(String email);
+
+}
