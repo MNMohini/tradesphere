d719150a8815fc1aae90fbffe76c4f2111a9ccdf
mohini <it.intern@bngroupindia.com>
2026-06-18 14:16:40 +0530

    Added some attributes in Employee.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/model/Employee.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/model/Employee.java
@@ -1,4 +1,32 @@
 package com.bnagritech.tradesphere.employee.model;
 
+import com.bnagritech.tradesphere.common.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "Employee")
 public class Employee {
+    @Id
+    private String id;
+    private String employeeId;
+    private String userName;
+    private String employeeName;
+    private String email;
+    private long phoneNumber;
+    private UserRole role;
+    private String territoryId;
+    private String joiningDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
+
