c32b08a253a8b10d68d447fae9ebfe96a71bb8e4
mohini <it.intern@bngroupindia.com>
2026-06-18 14:28:31 +0530

    added some attributes EmployeeRequest.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeRequest.java
@@ -1,4 +1,21 @@
 package com.bnagritech.tradesphere.employee.dto;
 
+import com.bnagritech.tradesphere.common.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeRequest {
+
+    private String employeeId;
+    private String employeeName;
+    private String email;
+    private String phoneNumber;
+    private UserRole role;
+    private String territoryId;
 }
