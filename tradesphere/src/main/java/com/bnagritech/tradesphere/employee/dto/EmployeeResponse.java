ed260c98ce991d630b60b994cc4480390cad5a8d
mohini <it.intern@bngroupindia.com>
2026-06-18 14:34:30 +0530

    Created EmployeeResponse.java class  added some attributes EmployeeRequest.java class

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeResponse.java
@@ -0,0 +1,29 @@
+package com.bnagritech.tradesphere.employee.dto;
+
+import com.bnagritech.tradesphere.common.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeResponse {
+    private String id;
+    private String employeeId;
+    private String userName;
+    private String employeeName;
+    private String email;
+    private long phoneNumber;
+    private UserRole role;
+    private String territoryId;
+    private String joiningDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+}
