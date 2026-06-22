8abb2b3bc6034a86ac79ee133b60c58267b0e48f
mohini <it.intern@bngroupindia.com>
2026-06-22 12:00:59 +0530

    added some attributes and annotation in RolePermission.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/model/RolePermission.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/model/RolePermission.java
@@ -1,5 +1,21 @@
 package com.bnagritech.tradesphere.roleAndPermssion.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+@Document(collection = "role_Permissions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RolePermission {
+
+    private String permissionId;
+    private String roleId;
+    private Set<Permission> permissions;
 
 }
