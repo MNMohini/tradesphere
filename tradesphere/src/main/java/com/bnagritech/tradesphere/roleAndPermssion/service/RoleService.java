811afddcd70fa9d2da48f3e9e1fd3d114b694898
mohini <it.intern@bngroupindia.com>
2026-06-22 12:51:45 +0530

    added methods in RoleService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/service/RoleService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/service/RoleService.java
@@ -1,5 +1,12 @@
 package com.bnagritech.tradesphere.roleAndPermssion.service;
 
+import com.bnagritech.tradesphere.roleAndPermssion.dto.CreateRoleRequest;
+import com.bnagritech.tradesphere.roleAndPermssion.model.Role;
+
+import java.util.List;
+
 public interface RoleService {
 
+        Role createRole(CreateRoleRequest request);
+        List <Role> allRoles();
 }
