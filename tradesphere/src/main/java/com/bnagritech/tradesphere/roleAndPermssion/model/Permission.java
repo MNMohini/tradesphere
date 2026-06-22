3b2248cf1d99013a18ad3d188637daca91acb56c
mohini <it.intern@bngroupindia.com>
2026-06-22 12:13:20 +0530

    Added some enums in Permission.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/model/Permission.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/model/Permission.java
@@ -2,5 +2,19 @@ package com.bnagritech.tradesphere.roleAndPermssion.model;
 
 public enum Permission {
 
+    create_Employee,
+    update_Employee,
+    delete_Employee,
+    
+    create_Distributor,
+    update_Distributor,
+    delete_Distributor,
+
+    create_Outlet,
+    update_Outlet,
+    delete_Outlet,
+
+    create_Order,
+    view_Order
 
 }
