2a57d03ac796d26e79f4b68cbfae6e8dabc3284a
mohini <it.intern@bngroupindia.com>
2026-06-24 15:16:55 +0530

    Did some changes in CreateRoleRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/dto/CreateRoleRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/dto/CreateRoleRequest.java
@@ -1,11 +1,14 @@
 package com.bnagritech.tradesphere.roleAndPermssion.dto;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class CreateRoleRequest {
+    @Id
     private String roleId;
     private String roleName;
     private String description;
     private String employeeId;
+
 }
