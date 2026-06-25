1476d44e5ad329adcee60143d6005a0034727624
mohini <it.intern@bngroupindia.com>
2026-06-25 16:21:46 +0530

    Did Some changes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/territory/model/Territory.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/model/Territory.java
@@ -1,12 +1,18 @@
 package com.bnagritech.tradesphere.territory.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "territory")
 public class Territory {
     @Id
     private String id;
