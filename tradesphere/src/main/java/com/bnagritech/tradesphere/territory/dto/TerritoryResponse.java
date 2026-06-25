18a348ed4c6a395f97fb6e52daecc8506b98a9a9
mohini <it.intern@bngroupindia.com>
2026-06-25 12:47:59 +0530

    created TerritoryResponse.java class and added some attributes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/territory/dto/TerritoryResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/dto/TerritoryResponse.java
@@ -1,16 +1,22 @@
 package com.bnagritech.tradesphere.territory.dto;
 
 import com.bnagritech.tradesphere.territory.TerritoryType;
+import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 @Data
-public class TerritoryRequest {
-
+@Builder
+public class TerritoryResponse {
+    private String id;
     private String territoryId;
     private String territoryName;
+    private TerritoryType territoryType;
     private String state;
     private String city;
     private String description;
     private Boolean active;
-    private TerritoryType territoryType;
+    private LocalDateTime updateAt;
+    private LocalDateTime createdBy;
+    private String updatedAt;
 }
