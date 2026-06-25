9c4da97bfe8a2e62fc823686b20d7f0ba4ced31c
mohini <it.intern@bngroupindia.com>
2026-06-25 13:33:35 +0530

    created TerritoryRepository.java class and create some methods

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/repository/TerritoryRepository.java
@@ -0,0 +1,20 @@
+package com.bnagritech.tradesphere.territory.repository;
+
+import com.bnagritech.tradesphere.territory.model.Territory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TerritoryRepository extends MongoRepository<Territory,String> {
+
+    boolean existsTerritoryByTerritoryId(String territoryId);
+
+    Optional<Territory> findByTerritoryId(String territoryId);
+
+    boolean existsTerritoryByTerritoryName(String territoryName);
+
+    Optional<Territory> findByState(String state);
+
+}
