ed255b739512d6c8d88dab927f8c3b7f3fca649b
mohini <it.intern@bngroupindia.com>
2026-06-26 11:19:54 +0530

    Created ResourceNotFoundException.java exception class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/common/exception/ResourceNotFoundException.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package com.bnagritech.tradesphere.common.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+
+    }
 }
