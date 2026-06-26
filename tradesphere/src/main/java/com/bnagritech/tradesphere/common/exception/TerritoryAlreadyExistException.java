d6299dc04f139b783039ff68d9624152d7fc00f2
mohini <it.intern@bngroupindia.com>
2026-06-26 11:37:20 +0530

    created TerritoryAlreadyExistException.java class

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/exception/TerritoryAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.bnagritech.tradesphere.common.exception;
+
+public class TerritoryAlreadyExistException extends RuntimeException {
+    public TerritoryAlreadyExistException(String message) {
+        super(message);
+    }
+}
