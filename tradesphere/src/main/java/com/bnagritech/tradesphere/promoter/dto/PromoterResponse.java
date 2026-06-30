2b55fb369bca1f523fd06c470dc09ccb9cb35704
mohini <it.intern@bngroupindia.com>
2026-06-30 20:06:22 +0530

    added some annotation

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/dto/PromoterResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/dto/PromoterResponse.java
@@ -1,5 +1,14 @@
 package com.bnagritech.tradesphere.promoter.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromoterResponse {
     private String promoterId;
     private String promoterName;
