899c8e2d535b5231430f4520884017c0a23b18d1
mohini <it.intern@bngroupindia.com>
2026-06-30 20:02:24 +0530

    added some attributes in it

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/dto/PromoterRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/dto/PromoterRequest.java
@@ -2,4 +2,15 @@ package com.bnagritech.tradesphere.promoter.dto;
 
 public class PromoterRequest {
 
+    private String promoterId;
+    private String promoterName;
+    private String address;
+    private String employeeId;
+    private String phoneNumber;
+    private String email;
+    private String territoryId;
+    private String territoryName;
+    private String city;
+    private String state;
+    private String status;
 }
