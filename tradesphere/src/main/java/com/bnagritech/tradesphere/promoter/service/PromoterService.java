dab58a3fbb14b7a42d12f2dca40b64127765ee20
mohini <it.intern@bngroupindia.com>
2026-07-01 14:48:52 +0530

    added some methods in PromoterService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/service/PromoterService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/service/PromoterService.java
@@ -1,6 +1,27 @@
 package com.bnagritech.tradesphere.promoter.service;
 
+
+import com.bnagritech.tradesphere.promoter.dto.PromoterRequest;
+import com.bnagritech.tradesphere.promoter.dto.PromoterResponse;
+
+import java.util.List;
+
 public interface PromoterService {
+
+    PromoterResponse createPromoter(PromoterRequest request);
+    List <PromoterResponse> getAllPromoters();
+    PromoterResponse getPromoterById(String promoterId);
+    PromoterResponse updatePromoter(String promoterId, PromoterRequest request);
+    void deletePromoter (String promoterId);
+    PromoterResponse getPromoterPhoneNumber(String phoneNumber);
+    PromoterResponse getPromoterByEmail(String email);
+    List<PromoterResponse> getPromoterByTerritory(String territoryId);
+    List<PromoterResponse> getPromoterByStatus(Boolean status);
+
+
+
+
+
 
 
 }
