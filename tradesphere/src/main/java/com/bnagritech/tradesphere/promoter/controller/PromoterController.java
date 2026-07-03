b51014482a745ac888c7515999958e70e6cb8fef
mohini <it.intern@bngroupindia.com>
2026-07-03 11:39:48 +0530

    added createPromoter Method in PromoterController.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
@@ -1,7 +1,15 @@
 package com.bnagritech.tradesphere.promoter.controller;
 
+import com.bnagritech.tradesphere.employee.dto.EmployeeRequest;
+import com.bnagritech.tradesphere.employee.dto.EmployeeResponse;
+import com.bnagritech.tradesphere.promoter.dto.PromoterRequest;
+import com.bnagritech.tradesphere.promoter.dto.PromoterResponse;
 import com.bnagritech.tradesphere.promoter.repository.PromoterRepository;
+import com.bnagritech.tradesphere.promoter.service.PromoterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/promoters")
 @RequiredArgsConstructor
 public class PromoterController {
-   // private final PromoterRepository promoterRepository;
+    private final PromoterService promoterService;
+     private final PromoterRepository promoterRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<PromoterResponse> createPromoter(@RequestBody PromoterRequest request){
+        return ResponseEntity.ok(promoterService.createPromoter(request));
+    }
 
 }
