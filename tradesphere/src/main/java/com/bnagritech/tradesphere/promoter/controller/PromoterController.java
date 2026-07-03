c99b353e1564ffb5cf59bf6e7e56bbf021583f10
mohini <it.intern@bngroupindia.com>
2026-07-03 11:41:31 +0530

    added getAllPromoters Method in PromoterController.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
@@ -8,10 +8,9 @@ import com.bnagritech.tradesphere.promoter.repository.PromoterRepository;
 import com.bnagritech.tradesphere.promoter.service.PromoterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/promoters")
@@ -23,6 +22,11 @@ public class PromoterController {
     @PostMapping("/create")
     public ResponseEntity<PromoterResponse> createPromoter(@RequestBody PromoterRequest request){
         return ResponseEntity.ok(promoterService.createPromoter(request));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PromoterResponse>>getAllPromoter() {
+        return ResponseEntity.ok(promoterService.getAllPromoters());
     }
 
 }
