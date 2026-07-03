1432dc1b23eb8abe60184ea194d8902a268cc6fa
mohini <it.intern@bngroupindia.com>
2026-07-03 11:32:20 +0530

    added annotation in PromoterController.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/promoter/controller/PromoterController.java
@@ -1,7 +1,13 @@
 package com.bnagritech.tradesphere.promoter.controller;
 
 import com.bnagritech.tradesphere.promoter.repository.PromoterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/promoters")
+@RequiredArgsConstructor
 public class PromoterController {
    // private final PromoterRepository promoterRepository;
 
