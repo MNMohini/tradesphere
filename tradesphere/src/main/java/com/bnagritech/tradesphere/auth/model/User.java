cb36d7d735755415de02a4681754d31ef4a775b1
mohini <it.intern@bngroupindia.com>
2026-07-03 18:40:47 +0530

    added some fields in User.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/model/User.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/model/User.java
@@ -2,7 +2,9 @@ package com.bnagritech.tradesphere.auth.model;
 
 import com.bnagritech.tradesphere.common.enums.UserRole;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -15,13 +17,44 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     private String id;
+
+    //Login
     private String userName;
     private String password;
+
+    //contact details
+    private String email;
+    private String phoneNumber;
+
+    //Personal
+    private String firstName;
+    private String lastName;
+    private String profileImage;
+
+    //mapping
     private String employeeId;
+    private String promoterId;
+    private String territoryId;
+
+    //Role
     private UserRole role;
+
+    //account status
     private Boolean active;
+    @Builder.Default
+    private Boolean verified= false;
+    @Builder.Default
+    private Boolean accountLocked= false;
+    @Builder.Default
+    private Integer failedLoginAttempts = 0;
+
+    //audit
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updateAt;
+    private String createdBy;
+    private String updatedBy;
 
 
 
