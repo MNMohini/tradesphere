476c3d112b7193507f0ffb7131df6c1b3b0cddfa
mohini <it.intern@bngroupindia.com>
2026-07-06 12:55:06 +0530

    created exception ResourceAlreadyExistsException.java

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.bnagritech.tradesphere.common.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
