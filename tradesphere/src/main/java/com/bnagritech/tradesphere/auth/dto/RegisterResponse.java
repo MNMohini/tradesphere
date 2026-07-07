27aad5120d6754c4cb830fb0f76b6050ef8d38a3
mohini <it.intern@bngroupindia.com>
2026-07-07 17:58:36 +0530

    refactor RegisterResponse.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterResponse.java
@@ -4,12 +4,26 @@ import com.bnagritech.tradesphere.common.enums.UserRole;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class RegisterResponse {
     private String id;
+    //Login
     private String userName;
-    private UserRole role;
+    //contact details
+    private String email;
+    private String phoneNumber;
+    //Personal
+    private String firstName;
+    private String lastName;
+    private String profileImageUrl;
+    //mapping
     private String employeeId;
+    //Role
+    private UserRole role;
     private Boolean active;
+    private Boolean verified;
+    private LocalDateTime createdAt;
 }
