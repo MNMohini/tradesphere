390a225b45a54b47b7d70e544df9f50be054620b
mohini <it.intern@bngroupindia.com>
2026-07-07 17:51:55 +0530

    did some changes in RegisterRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/auth/dto/RegisterRequest.java
@@ -6,15 +6,29 @@ import lombok.Data;
 
 @Data
 public class RegisterRequest {
-
+    //login
     @NotBlank(message = "Username is required")
     private  String userName;
-
     @NotBlank(message = "password is required")
     private String password;
 
+    //contact
+    @NotBlank(message = "Required")
+    private String email;
+    @NotBlank(message = "Required")
+    private String phoneNumber;
+
+    //mapping
     @NotBlank(message = "Employee is required")
     private String employeeId;
+
+    //personal
+    @NotBlank(message = "Required")
+    private String firstName;
+    @NotBlank(message = "Required")
+    private String lastName;
+    @NotBlank(message = "Required")
+    private String profileImageUrl;
 
     @NotBlank(message = "Role is required")
     private UserRole role;
