db21332ca9c6aa4f4570c03a75e06206fe62cb3d
mohini <it.intern@bngroupindia.com>
2026-07-10 11:57:55 +0530

    created BeatOwnerType.java

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/BeatOwnerType.java
@@ -0,0 +1,8 @@
+package com.bnagritech.tradesphere.common.enums;
+
+public enum BeatOwnerType {
+    STATE_MANAGER,
+    AREA_MANAGER,
+    DISTRICT_MANAGER,
+    PROMOTER
+}
--- tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/RetailerType.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/RetailerType.java
@@ -3,13 +3,11 @@ package com.bnagritech.tradesphere.common.enums;
 public enum RetailerType {
     //B TO B
     WHOLESALER,
-
     // B TO C
     GENERAL_STORE,
     SUPER_MARKET,
     DISTRIBUTOR_RETAILER,
     E_COMMERCE,
-
     //canteen stores department
     CSD
 }
