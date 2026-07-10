82a88bd885c777b2ae35a950023f66fd67299937
mohini <it.intern@bngroupindia.com>
2026-07-10 14:08:12 +0530

    created BeatController.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/beat/controller/BeatController.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/controller/BeatController.java
@@ -1,4 +1,12 @@
 package com.bnagritech.tradesphere.beat.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/beats")
+@RequiredArgsConstructor
 public class BeatController {
+
 }
