8dc451005c31a001b7cb13b0f6328ea6f1cb55c2
mohini <it.intern@bngroupindia.com>
2026-07-10 13:15:12 +0530

    created methods in BeatService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/beat/service/BeatService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/service/BeatService.java
@@ -1,5 +1,39 @@
 package com.bnagritech.tradesphere.beat.service;
 
+import com.bnagritech.tradesphere.beat.dto.BeatsRequest;
+import com.bnagritech.tradesphere.beat.dto.BeatsResponse;
+import com.bnagritech.tradesphere.common.enums.ApprovalStatus;
+import com.bnagritech.tradesphere.common.enums.BeatDay;
+import com.bnagritech.tradesphere.common.enums.BeatStatus;
+import com.bnagritech.tradesphere.common.enums.RetailerType;
+
+import java.util.List;
+
 public interface BeatService {
+    // BASIC CRUD
+    BeatsResponse createBeat(BeatsRequest request);
+    BeatsResponse updateBeat(String beatId, BeatsRequest request);
+    BeatsResponse getBeatById(String beatId);
+    List<BeatsResponse> getAllBeats();
+    void deleteBeat(String beatId);
+    // TERRITORY
+    List<BeatsResponse> getBeatsByTerritory(String territoryId);
+    //EMPLOYEE BEATS
+    List<BeatsResponse> getEmployeeBeats(String employeeId);
+    List<BeatsResponse> getEmployeeDayBeats(String employeeId, BeatDay beatDay);
+    // RETAILER MAPPING
+    BeatsResponse assignRetailerToBeat(String beatId, String retailerId);
+    BeatsResponse removeRetailerFromBeat(String beatId, String retailerId);
+    BeatsResponse getBeatByRetailer(String retailerId);
+    // APPROVAL
+    BeatsResponse approveBeat(String beatId, String managerId);
+    BeatsResponse rejectBeat(String beatId, String managerId);
+    // SEARCH
+    List<BeatsResponse> searchByCity(String city);
+    List<BeatsResponse> searchByState(String state);
+    // DASHBOARD FILTERS
+    List<BeatsResponse> getByStatus(BeatStatus status);
+    List<BeatsResponse> getByBeatType(RetailerType beatType);
+    List<BeatsResponse> getByApprovalStatus(ApprovalStatus approvalStatus);
 
 }
