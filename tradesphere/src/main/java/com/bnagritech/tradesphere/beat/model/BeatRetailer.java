a2b761c7c5c904c20a6cf30c313e80f30c8cbd9d
mohini <it.intern@bngroupindia.com>
2026-07-10 12:54:20 +0530

    added annotation in BeatRetailer.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/beat/model/BeatRetailer.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/model/BeatRetailer.java
@@ -1,7 +1,16 @@
 package com.bnagritech.tradesphere.beat.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalTime;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BeatRetailer {
     // Retailer Reference
     private String retailerId;
