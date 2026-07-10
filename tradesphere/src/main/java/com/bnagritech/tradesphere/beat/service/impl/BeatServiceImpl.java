9691e9572b7e15a20163527abe0f2a1d694ccb8a
mohini <it.intern@bngroupindia.com>
2026-07-10 16:10:20 +0530

    created deleted method

--- tradesphere/src/main/java/com/bnagritech/tradesphere/beat/service/impl/BeatServiceImpl.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/service/impl/BeatServiceImpl.java
@@ -2,23 +2,92 @@ package com.bnagritech.tradesphere.beat.service.impl;
 
 import com.bnagritech.tradesphere.beat.dto.BeatsRequest;
 import com.bnagritech.tradesphere.beat.dto.BeatsResponse;
+import com.bnagritech.tradesphere.beat.model.Beat;
+import com.bnagritech.tradesphere.beat.repository.BeatRepository;
 import com.bnagritech.tradesphere.beat.service.BeatService;
 import com.bnagritech.tradesphere.common.enums.ApprovalStatus;
 import com.bnagritech.tradesphere.common.enums.BeatDay;
 import com.bnagritech.tradesphere.common.enums.BeatStatus;
 import com.bnagritech.tradesphere.common.enums.RetailerType;
+import com.bnagritech.tradesphere.common.exception.EmployeeNotFoundException;
+import com.bnagritech.tradesphere.common.exception.ResourceAlreadyExistsException;
+import com.bnagritech.tradesphere.common.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
 public class BeatServiceImpl implements BeatService {
+    private final BeatRepository beatRepository;
+
     @Override
     public BeatsResponse createBeat(BeatsRequest request) {
-        return null;
+        if(beatRepository.existsByBeatNameAndTerritoryIdAndBeatCode(
+                request.getBeatName(), request.getTerritoryId(), request.getBeatCode())){
+            throw new ResourceAlreadyExistsException("Beat already exists");
+        }
+        Beat beat = Beat.builder()
+                .beatName(request.getBeatName())
+                .beatCode(request.getBeatCode())
+                .description(request.getDescription())
+                .beatType(request.getBeatType())
+                .territoryId(request.getTerritoryId())
+                .assignedEmployeeId(request.getAssignedEmployeeId())
+                .beatOwnerType(request.getBeatOwnerType())
+                .managerId(request.getManagerId())
+                .state(request.getState())
+                .city(request.getCity())
+                .area(request.getArea())
+                .pinCode(request.getPinCode())
+                .latitude(request.getLatitude())
+                .longitude(request.getLongitude())
+                .retailers(request.getRetailers())
+                .totalRetailers(request.getTotalRetailers())
+                .beatDays(request.getBeatDays())
+                .frequency(request.getFrequency())
+                .effectiveFrom(request.getEffectiveFrom())
+                .effectiveTo(request.getEffectiveTo())
+                .estimatedDistanceKm(request.getEstimatedDistanceKm())
+                .estimatedTravelTimeMinutes(request.getEstimatedTravelTimeMinutes())
+                .approvalStatus(request.getApprovalStatus())
+                .build();
+        Beat beatSaved = beatRepository.save(beat);
+        return mapToResponse(beatSaved);
     }
 
     @Override
     public BeatsResponse updateBeat(String beatId, BeatsRequest request) {
-        return null;
+      Beat beat = beatRepository.findByBeatId(beatId)
+              .orElseThrow(() -> new ResourceNotFoundException("beat not found"));
+
+                beat.setBeatName(request.getBeatName());
+                beat.setBeatCode(request.getBeatCode());
+                beat.setDescription(request.getDescription());
+                beat.setBeatType(request.getBeatType());
+                beat.setTerritoryId(request.getTerritoryId());
+                beat.setAssignedEmployeeId(request.getAssignedEmployeeId());
+                beat.setBeatOwnerType(request.getBeatOwnerType());
+                beat.setManagerId(request.getManagerId());
+                beat.setState(request.getState());
+                beat.setCity(request.getCity());
+                beat.setArea(request.getArea());
+                beat.setPinCode(request.getPinCode());
+                beat.setLatitude(request.getLatitude());
+                beat.setLongitude(request.getLongitude());
+                beat.setRetailers(request.getRetailers());
+                beat.setTotalRetailers(request.getTotalRetailers());
+                beat.setBeatDays(request.getBeatDays());
+                beat.setFrequency(request.getFrequency());
+                beat.setEffectiveFrom(request.getEffectiveFrom());
+                beat.setEffectiveTo(request.getEffectiveTo());
+                beat.setEstimatedDistanceKm(request.getEstimatedDistanceKm());
+                beat.setEstimatedTravelTimeMinutes(request.getEstimatedTravelTimeMinutes());
+                beat.setApprovalStatus(request.getApprovalStatus());
+
+        Beat beatUpdated = beatRepository.save(beat);
+        return mapToResponse(beatUpdated);
     }
 
     @Override
@@ -34,8 +103,17 @@ public class BeatServiceImpl implements BeatService {
     @Override
     public void deleteBeat(String beatId) {
 
-    }
+            Beat beat =
+                    beatRepository
+                            .findByBeatId(beatId)
+                            .orElseThrow(
+                                    () -> new RuntimeException("Beat not found")
+                            );
 
+
+        beatRepository.delete(beat);
+
+    }
     @Override
     public List<BeatsResponse> getBeatsByTerritory(String territoryId) {
         return List.of();
@@ -99,5 +177,30 @@ public class BeatServiceImpl implements BeatService {
     @Override
     public List<BeatsResponse> getByApprovalStatus(ApprovalStatus approvalStatus) {
         return List.of();
+    }
+    private BeatsResponse mapToResponse(Beat beat) {
+
+        return BeatsResponse.builder()
+                .beatId(beat.getBeatId())
+                .beatName(beat.getBeatName())
+                .beatCode(beat.getBeatCode())
+                .description(beat.getDescription())
+                .beatType(beat.getBeatType())
+                .territoryId(beat.getTerritoryId())
+                .assignedEmployeeId(beat.getAssignedEmployeeId())
+                .beatOwnerType(beat.getBeatOwnerType())
+                .state(beat.getState())
+                .city(beat.getCity())
+                .area(beat.getArea())
+                .retailers(beat.getRetailers())
+                .totalRetailers(beat.getTotalRetailers())
+                .beatDays(beat.getBeatDays())
+                .frequency(beat.getFrequency())
+                .approvalStatus(beat.getApprovalStatus())
+                .status(beat.getStatus())
+                .createdAt(beat.getCreatedAt())
+                .updatedAt(beat.getUpdatedAt())
+                .build();
+
     }
 }
