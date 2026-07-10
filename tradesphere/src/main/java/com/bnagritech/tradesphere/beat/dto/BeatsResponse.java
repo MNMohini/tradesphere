bce9be58e9748386a5211ea746aa8411d52b38e4
mohini <it.intern@bngroupindia.com>
2026-07-10 13:14:39 +0530

    refactor the code

--- tradesphere/src/main/java/com/bnagritech/tradesphere/beat/dto/BeatsResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/beat/dto/BeatsResponse.java
@@ -2,12 +2,14 @@ package com.bnagritech.tradesphere.beat.dto;
 
 import com.bnagritech.tradesphere.beat.model.BeatRetailer;
 import com.bnagritech.tradesphere.common.enums.*;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 @Data
+@Builder
 public class BeatsResponse {
 
     // Basic Details
