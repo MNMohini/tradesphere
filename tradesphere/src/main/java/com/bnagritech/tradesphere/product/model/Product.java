d30fdc0fceefe80de692b7082d3e0fda6db207ca
mohini <it.intern@bngroupindia.com>
2026-07-17 12:11:58 +0530

    created Product.java added fields in it and annotated the class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/product/model/Product.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/product/model/Product.java
@@ -1,5 +1,16 @@
 package com.bnagritech.tradesphere.product.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "products")
 public class Product {
     private int id;
     private String productName;
