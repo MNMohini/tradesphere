51048df39d6c2f0089f2053dd6f6252d2c5b7b09
mohini <it.intern@bngroupindia.com>
2026-07-21 15:27:20 +0530

    added some method in ProductController.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/product/controller/ProductController.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/product/controller/ProductController.java
@@ -1,13 +1,14 @@
 package com.bnagritech.tradesphere.product.controller;
+import com.bnagritech.tradesphere.employee.dto.EmployeeRequest;
+import com.bnagritech.tradesphere.employee.dto.EmployeeResponse;
 import com.bnagritech.tradesphere.product.dto.ProductRequest;
 import com.bnagritech.tradesphere.product.dto.ProductResponse;
 import com.bnagritech.tradesphere.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -18,5 +19,28 @@ public class ProductController {
     @PostMapping("/create")
     public ResponseEntity<ProductResponse> addProduct(@RequestBody ProductRequest request){
         return ResponseEntity.ok(productService.addProduct(request));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductResponse>>getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+    @PutMapping("/Id/{productId}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable String productId,
+            @RequestBody ProductRequest request)
+    {
+        return ResponseEntity.ok(productService.updateProduct(productId,request));
+    }
+    @GetMapping("/Id/{productId}")
+    public ResponseEntity<ProductResponse> getProductByProductId(@PathVariable String productId){
+        return ResponseEntity.ok(productService.getProductByProductId(productId));
+    }
+    @GetMapping("/name/{productName}")
+    public ResponseEntity<ProductResponse> getProductByProductName(@PathVariable String productName){
+        return ResponseEntity.ok(productService.getProductByProductName(productName));
+    }
+    @GetMapping("/Id/{skuCode}")
+    public ResponseEntity<ProductResponse> getProductBySkuCode(@PathVariable String skuCode){
+        return ResponseEntity.ok(productService.getProductByProductSkuCode(skuCode));
     }
 }
