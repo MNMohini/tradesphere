0960b3711f9a5edc5f5dddb2e0b12f9498b800c0
mohini <it.intern@bngroupindia.com>
2026-07-24 13:39:14 +0530

    added annotations in DTOs

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitStatusUpdateRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitStatusUpdateRequest.java
@@ -1,4 +1,12 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VisitStatusUpdateRequest {
+
 }
