7337d43f56b97b56b87526c7d519a550b93f233e
mohini <it.intern@bngroupindia.com>
2026-07-24 13:48:54 +0530

    created some methods in VisitService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/service/VisitService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/service/VisitService.java
@@ -1,8 +1,22 @@
 package com.bnagritech.tradesphere.visit.service;
 
+import com.bnagritech.tradesphere.visit.dto.*;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface VisitService {
+
+    VisitResponse createVisit(VisitRequest request);
+    VisitResponse updateVisit(String id, VisitRequest request);
+    VisitResponse getVisitById(String id);
+    List<VisitResponse> getAllVisits();
+    List<VisitResponse> getVisitsByEmployee(String employeeId);
+    List<VisitResponse> getVisitsByRetailer(String retailerId);
+    VisitResponse checkIn(VisitCheckInRequest request);
+    VisitResponse checkOut(VisitCheckOutResponse response);
+    VisitResponse updateVisitStatus(String id, VisitStatusUpdateRequest request);
+    void deleteVisit(String id);
 
 }
