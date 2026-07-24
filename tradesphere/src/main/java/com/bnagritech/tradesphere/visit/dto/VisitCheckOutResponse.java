fdbb7d4c2728cf9994b6db3e1363e0d1c28f019a
mohini <it.intern@bngroupindia.com>
2026-07-24 13:19:17 +0530

    Added more fields in VisitCheckOutRequest.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitCheckOutResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitCheckOutResponse.java
@@ -5,10 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class VisitCheckOutResponse {
-
+    private String employeeId;
+    private String beatId;
+    private String territoryId;
+    private String pictureUrl;
+    private Double longitude;
+    private Double latitude;
+    private LocalDateTime checkInDate;
+    private LocalDateTime checkOutDate;
+    private String visitId;
 }
