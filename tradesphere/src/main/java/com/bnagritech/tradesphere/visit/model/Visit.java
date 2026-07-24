4f5eef454ed88dce475b6f601960ca1ffc663b36
mohini <it.intern@bngroupindia.com>
2026-07-24 12:04:51 +0530

    added fields in Visit.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/model/Visit.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/model/Visit.java
@@ -1,8 +1,13 @@
 package com.bnagritech.tradesphere.visit.model;
 
+import com.bnagritech.tradesphere.common.VisitType;
+import com.bnagritech.tradesphere.common.enums.VisitOutCome;
+import com.bnagritech.tradesphere.common.enums.VisitStatus;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -12,5 +17,34 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Visit {
     @Id
     private String id;
+    private String visitCode;
+    private String employeeId;
+    private String beatId;
+    private String retailerId;
+    private String cityId;
+    private String cityName;
+    private String countryId;
+    private String countryName;
+    private String territoryId;
+    private LocalDateTime plannedDate;
+    private LocalDateTime checkInDate;
+    private LocalDateTime checkOutDate;
+    private Double longitude;
+    private Double latitude;
+    private Double gpsAccuracy;
+    private VisitStatus visitStatus;
+    private VisitType visitType;
+    private VisitOutCome visitOutCome;
+    private String remarks;
+    private Integer durationInMinutes;
+    private Boolean orderBooked;
+    private Boolean stockVerified;
+    private Boolean merchandisingDone;
+    private Boolean paymentCollected;
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
 
 }
