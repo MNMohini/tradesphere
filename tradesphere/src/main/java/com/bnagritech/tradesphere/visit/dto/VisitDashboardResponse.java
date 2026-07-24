4d4a7d47060e57870bfea87dbc1da32cd44eb0cd
mohini <it.intern@bngroupindia.com>
2026-07-24 13:38:54 +0530

    added annotations

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitDashboardResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitDashboardResponse.java
@@ -1,5 +1,14 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class VisitDashboardResponse {
 
 }
