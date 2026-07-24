0250567c17431a68a3dac99a45b6ef1b7005df88
mohini <it.intern@bngroupindia.com>
2026-07-24 13:36:18 +0530

    updated

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitFilterRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitFilterRequest.java
@@ -1,11 +1,22 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import com.bnagritech.tradesphere.common.enums.VisitStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VisitFilterRequest {
+    private String employeeId;
+    private String retailerId;
+    private String beatId;
+    private String territoryId;
+    private VisitStatus visitStatus;
+    private LocalDate fromDate;
+    private LocalDate toDate;
 }
