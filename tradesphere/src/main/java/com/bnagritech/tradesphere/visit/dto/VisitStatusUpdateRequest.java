afc6342777a50d5fe138f9a305d350098e447333
mohini <it.intern@bngroupindia.com>
2026-07-24 13:41:44 +0530

    added fields in VisitStatusUpdateRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitStatusUpdateRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitStatusUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import com.bnagritech.tradesphere.common.enums.VisitOutCome;
+import com.bnagritech.tradesphere.common.enums.VisitStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,4 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class VisitStatusUpdateRequest {
 
+    private VisitStatus status;
+    private VisitOutCome visitOutcome;
+    private String remarks;
 }
