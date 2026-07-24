a0f55370c957d120e379b7ee5da6e22fa9b2b285
mohini <it.intern@bngroupindia.com>
2026-07-24 12:03:38 +0530

    create VisitOutCome.java ENUM

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/VisitOutCome.java
@@ -0,0 +1,11 @@
+package com.bnagritech.tradesphere.common.enums;
+
+public enum VisitOutCome {
+    ORDER_BOOKED,
+    NO_ORDER,
+    OUTLET_CLOSED,
+    OWNER_UNAVAILABLE,
+    PAYMENT_COLLECTED,
+    FOLLOWUP_REQUIRED,
+    PAYMENT_PENDING
+}
