442600254e4144f8e5052a40530a3dc52a91366a
mohini <it.intern@bngroupindia.com>
2026-07-24 13:03:58 +0530

    added annotations

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitRequest.java
@@ -3,9 +3,14 @@ package com.bnagritech.tradesphere.visit.dto;
 import com.bnagritech.tradesphere.common.VisitType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VisitRequest {
     @NotBlank(message = "Employee Id is required")
     private String employeeId;
