a46ffe3dab108aa6aa49d9911c6771bd19b7e1b8
mohini <it.intern@bngroupindia.com>
2026-07-24 13:14:53 +0530

    added fields and annotations in VisitCheckInRequest

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitCheckInRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitCheckInRequest.java
@@ -1,4 +1,18 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisitCheckInRequest {
+    private String checkinDate;
+    private String checkoutDate;
+    private  String visitId;
+    private String employeeId;
+    private String beatId;
+    private String TerritoryId;
 }
