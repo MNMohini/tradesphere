24e633fc33b48abef2606df4706edd56eb963031
mohini <it.intern@bngroupindia.com>
2026-07-24 13:04:29 +0530

    Added fields and annotations

--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/dto/VisitResponse.java
@@ -1,4 +1,48 @@
 package com.bnagritech.tradesphere.visit.dto;
 
+import com.bnagritech.tradesphere.common.VisitType;
+import com.bnagritech.tradesphere.common.enums.VisitOutCome;
+import com.bnagritech.tradesphere.common.enums.VisitStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisitResponse {
+    private String id;
+    private String visitCode;
+
+    private String employeeId;
+    private String retailerId;
+    private String beatId;
+    private String territoryId;
+
+    private LocalDateTime plannedDate;
+
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+
+    private VisitStatus status;
+    private VisitType visitType;
+    private VisitOutCome visitOutcome;
+
+    private Integer durationInMinutes;
+
+    private Boolean orderBooked;
+    private Boolean stockVerified;
+    private Boolean merchandisingDone;
+    private Boolean paymentCollected;
+
+    private String remarks;
+
+    private String createdBy;
+    private String updatedBy;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
