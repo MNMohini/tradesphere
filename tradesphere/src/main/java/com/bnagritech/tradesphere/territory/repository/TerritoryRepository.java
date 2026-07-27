8ed9d942e54a680622b427d01dcab7559f6673f1
mohini <it.intern@bngroupindia.com>
2026-07-27 14:59:40 +0530

    updated

--- tradesphere/src/main/java/com/bnagritech/tradesphere/territory/repository/TerritoryRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/repository/TerritoryRepository.java
@@ -2,12 +2,9 @@ package com.bnagritech.tradesphere.territory.repository;
 
 import com.bnagritech.tradesphere.territory.model.Territory;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-@Repository
 public interface TerritoryRepository extends MongoRepository<Territory,String> {
 
     boolean existsTerritoryByTerritoryId(String territoryId);
--- tradesphere/src/main/java/com/bnagritech/tradesphere/visit/repository/VisitRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/visit/repository/VisitRepository.java
@@ -3,13 +3,10 @@ package com.bnagritech.tradesphere.visit.repository;
 import com.bnagritech.tradesphere.common.enums.VisitStatus;
 import com.bnagritech.tradesphere.visit.model.Visit;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
-@Repository
 public interface VisitRepository extends MongoRepository<Visit, String> {
     Optional<Visit> findByVisitCode(String visitCode);
     List<Visit> findByEmployeeId(String employeeId);
