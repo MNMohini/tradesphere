0fa8541003bda157b8843649f9b4cc0993362afc
mohini <it.intern@bngroupindia.com>
2026-07-29 11:54:10 +0530

    did some changes

--- tradesphere/src/main/java/com/bnagritech/tradesphere/auth/service/OTPService.java
+++
@@ -1,6 +0,0 @@
-package com.bnagritech.tradesphere.auth.service;
-
-public interface OTPService {
-    void sendOTP(String phoneNumber);
-    boolean verifyOtp(String phoneNumber, String resetPasswordOTP);
-}
