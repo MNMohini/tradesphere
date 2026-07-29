fbe0b682efa2ac76a4b53bb470d5e6badc7e39ed
mohini <it.intern@bngroupindia.com>
2026-07-29 12:20:29 +0530

    add AdminInitializer.java  class

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/security/config/AdminInitializer.java
@@ -0,0 +1,40 @@
+package com.bnagritech.tradesphere.security.config;
+
+import com.bnagritech.tradesphere.auth.model.User;
+import com.bnagritech.tradesphere.auth.repository.UserRepository;
+import com.bnagritech.tradesphere.common.enums.UserRole;
+import com.bnagritech.tradesphere.common.enums.UserStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Configuration
+@RequiredArgsConstructor
+public class AdminInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    CommandLineRunner initAdmin() {
+        return args -> {
+            if (!userRepository.existsByUserName("Mohini01")) {
+                User admin = User.builder()
+                        .userName("Mohini01")
+                        .employeeId("EMP01")
+                        .email("it.intern@bngroupindia.com")
+                        .password(passwordEncoder.encode("mohini@bngroup"))
+                        .role(UserRole.ADMIN)
+                        .status(UserStatus.ACTIVE)
+                        .enabled(true)
+                        .accountLocked(false)
+                        .failedLoginAttempts(0)
+                        .build();
+                userRepository.save(admin);
+
+                System.out.println("Admin user created successfully.");
+            }
+        };
+    }
+}
