05566f566be0b6d7906b471ae3c52d8497c7b0a0
mohini <it.intern@bngroupindia.com>
2026-07-29 18:30:49 +0530

    added CorsConfig.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/security/config/CorsConfig.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/security/config/CorsConfig.java
@@ -1,12 +1,13 @@
 package com.bnagritech.tradesphere.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
-
+@Configuration
 public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
