ef99bf08752fe80f0a1633016daec84a33c093b2
mohini <it.intern@bngroupindia.com>
2026-07-29 12:06:47 +0530

    updated employee module

--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/model/Employee.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/model/Employee.java
@@ -1,5 +1,6 @@
 package com.bnagritech.tradesphere.employee.model;
 
+import com.bnagritech.tradesphere.common.enums.EmployeeStatus;
 import com.bnagritech.tradesphere.common.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,8 +27,14 @@ public class Employee {
     private String profileImageUrl;
     private String email;
     private String territoryId;
+    private EmployeeStatus employeeStatus;
+    private String managerName1;
+    private String managerName2;
+    private String designation;
     private String joiningDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
 }
 
