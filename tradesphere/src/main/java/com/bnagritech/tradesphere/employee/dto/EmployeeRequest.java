79737335aa2ca7d6dfa587b779b339ab833832fe
mohini <it.intern@bngroupindia.com>
2026-07-30 11:48:59 +0530

    dtos

--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeRequest.java
@@ -1,22 +1,31 @@
 package com.bnagritech.tradesphere.employee.dto;
 
+import com.bnagritech.tradesphere.common.enums.EmployeeStatus;
 import com.bnagritech.tradesphere.common.enums.UserRole;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class EmployeeRequest {
     private String employeeId;
-    private String employeeName;
     private String userName;
-    private String email;
-    private String phoneNumber;
+    private String employeeName;
     private UserRole role;
+    private String phoneNumber;
+    private String profileImageUrl;
+    private String email;
     private String territoryId;
+    private EmployeeStatus employeeStatus;
+    private String managerName1;
     private String dateOfBirth;
+    private String managerName2;
     private String designation;
     private String joiningDate;
-    private String managerName1;
-    private String managerName2;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
 
 
 }
--- tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/employee/dto/EmployeeResponse.java
@@ -1,5 +1,6 @@
 package com.bnagritech.tradesphere.employee.dto;
 
+import com.bnagritech.tradesphere.common.enums.EmployeeStatus;
 import com.bnagritech.tradesphere.common.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,13 +19,21 @@ public class EmployeeResponse {
     private String employeeId;
     private String userName;
     private String employeeName;
-    private String email;
-    private String phoneNumber;
     private UserRole role;
+    private String phoneNumber;
+    private String profileImageUrl;
+    private String email;
     private String territoryId;
+    private EmployeeStatus employeeStatus;
+    private String managerName1;
+    private String dateOfBirth;
+    private String managerName2;
+    private String designation;
     private String joiningDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private String createdBy;
+    private String updatedBy;
 
 
 }
