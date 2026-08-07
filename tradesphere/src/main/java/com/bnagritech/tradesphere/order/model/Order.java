d4dda8f22c3bbc45a5bbd4c469eb381f31aab43c
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-07 13:35:45 +0530

    added more fields in Order.java class

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/Order.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/Order.java
@@ -1,5 +1,9 @@
 package com.bnagritech.tradesphere.order.model;
 
+import com.bnagritech.tradesphere.common.enums.OrderStatus;
+import com.bnagritech.tradesphere.common.enums.OrderType;
+import com.bnagritech.tradesphere.common.enums.PaymentMode;
+import com.bnagritech.tradesphere.common.enums.PaymentStatus;
 import com.bnagritech.tradesphere.product.model.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +11,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -16,12 +23,39 @@ import java.util.List;
 @Document(collection = "orders")
 public class Order {
     private String id;
-    private String orderId;
+    private String orderNumber;
+
+    private String employeeId;
     private String retailerId;
-    private String retailerName;
-    private List<Product> quantity;
-    private String retailerFeedback;
-    private String totalBill;
-    private String outstandingBalance;
-    private String paidAmount;
+    private String promoterId;
+    private String beatId;
+    private String visitId;
+
+    private List<OrderItems> items;
+
+    private Integer totalQuantity;
+
+    private BigDecimal grossAmount;
+    private BigDecimal discountAmount;
+    private BigDecimal schemeDiscount;
+    private BigDecimal taxableAmount;
+    private BigDecimal taxAmount;
+    private BigDecimal netAmount;
+
+    private OrderType orderType;
+    private OrderStatus status;
+    private PaymentMode paymentMode;
+    private PaymentStatus paymentStatus;
+
+    private String remarks;
+    private String cancellationReason;
+
+    private LocalDate orderDate;
+    private LocalDate expectedDeliveryDate;
+
+    private String createdBy;
+    private String updatedBy;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
