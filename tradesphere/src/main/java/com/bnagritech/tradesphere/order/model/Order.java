cfd6cf265ffff7c7fb7dbde0c7a5d9e17eecd264
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-07 13:24:30 +0530

    added some fields in the Order.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/Order.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/Order.java
@@ -1,4 +1,27 @@
 package com.bnagritech.tradesphere.order.model;
 
+import com.bnagritech.tradesphere.product.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "orders")
 public class Order {
+    private String id;
+    private String orderId;
+    private String retailerId;
+    private String retailerName;
+    private List<Product> quantity;
+    private String retailerFeedback;
+    private String totalBill;
+    private String outstandingBalance;
+    private String paidAmount;
 }
