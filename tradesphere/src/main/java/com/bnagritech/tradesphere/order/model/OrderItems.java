eece9996d6769162f77bbb1817c7f0a8b3074c09
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-07 13:29:19 +0530

    added some fields in OrderItems.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/OrderItems.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/OrderItems.java
@@ -1,4 +1,36 @@
 package com.bnagritech.tradesphere.order.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItems {
+
+    private String skuCode;
+    private String productName;
+
+    private Integer orderedQuantity;
+    private Integer approvedQuantity;
+    private Integer deliveredQuantity;
+
+    private BigDecimal mrp;
+    private BigDecimal unitPrice;
+
+    private BigDecimal discountPercentage;
+    private BigDecimal discountAmount;
+
+    private BigDecimal taxPercentage;
+    private BigDecimal taxAmount;
+
+    private BigDecimal lineAmount;
+
+    private String schemeId;
+    private String schemeName;
 }
