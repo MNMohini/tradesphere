c64483d119e76519e892d60d52667d43a8d3260f
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-07 16:52:27 +0530

    added some methods in OrderRepository.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
@@ -1,4 +1,22 @@
 package com.bnagritech.tradesphere.order.repository;
 
-public interface OrderRepository {
+import com.bnagritech.tradesphere.order.model.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order,String> {
+
+    Optional<Order> findByOrderNumber(String orderNumber);
+    Optional<Order> findByRetailerId(String retailerId);
+    Optional<Order> findByPromoterId(String promoterId);
+    Optional<Order> findByPaymentStatus(String paymentStatus);
+
+    boolean existsByOrderNumber(String email);
+    boolean existsByRetailerId(String retailerId);
+    boolean existsByPromoterId(String promoterId);
+    boolean existsByEmployeeID(String employeeId);
+
 }
