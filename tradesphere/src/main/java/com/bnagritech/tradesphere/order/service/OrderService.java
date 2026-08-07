620fb73d9345ced5dfe11ea70db041d9eb308599
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-07 17:11:46 +0530

    wrote methods in OrderService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/service/OrderService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/service/OrderService.java
@@ -1,4 +1,19 @@
 package com.bnagritech.tradesphere.order.service;
 
+import com.bnagritech.tradesphere.order.dto.CreateOrderRequest;
+import com.bnagritech.tradesphere.order.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface OrderService {
+
+    OrderResponse createOrder(CreateOrderRequest request);
+    List<OrderResponse> getOrdersByEmployeeId();
+    OrderResponse getOrderByRetailerName(String RetailerName);
+
+    OrderResponse getOrdersByRetailerId(String retailerId);
+    OrderResponse getOrderByPromoterId(String promoterId);
+    OrderResponse updateOrder(String OrderNumber, CreateOrderRequest request);
 }
