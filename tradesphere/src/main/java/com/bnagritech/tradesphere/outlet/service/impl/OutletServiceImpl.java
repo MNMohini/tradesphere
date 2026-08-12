aa8d55f9faf74e4ae4268785666e26827569dc7f
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-12 14:42:35 +0530

    override the OutletService in OutletServiceImpl.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/outlet/service/impl/OutletServiceImpl.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/outlet/service/impl/OutletServiceImpl.java
@@ -1,4 +1,60 @@
 package com.bnagritech.tradesphere.outlet.service.impl;
 
-public class OutletServiceImpl {
+import com.bnagritech.tradesphere.outlet.dto.OutletRequest;
+import com.bnagritech.tradesphere.outlet.dto.OutletResponse;
+import com.bnagritech.tradesphere.outlet.service.OutletService;
+
+import java.util.List;
+
+public class OutletServiceImpl implements OutletService {
+
+    @Override
+    public OutletResponse createOutlet(OutletRequest request) {
+        return null;
+    }
+
+    @Override
+    public OutletResponse getOutletByOutletId(String outletId) {
+        return null;
+    }
+
+    @Override
+    public OutletResponse updateOutlet(String OutletId, OutletRequest request) {
+        return null;
+    }
+
+    @Override
+    public OutletResponse getOutletByPhoneNumber(String phoneNumber) {
+        return null;
+    }
+
+    @Override
+    public OutletResponse getOutletByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public OutletResponse updateOutletStatus(String outletId, OutletRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<OutletResponse> getOutletsByTerritoryId(String territoryId) {
+        return List.of();
+    }
+
+    @Override
+    public List<OutletResponse> getOutletsByOutletName(String outletName) {
+        return List.of();
+    }
+
+    @Override
+    public List<OutletResponse> getOutletsByPromoterId(String promoterId) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteOutlet(String outletId) {
+
+    }
 }
