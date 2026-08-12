47fdad72a9c77e2efdb077d595aaedb3cc60dd41
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-12 14:40:55 +0530

    added methods in OutletService.java interface

--- tradesphere/src/main/java/com/bnagritech/tradesphere/outlet/service/OutletService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/outlet/service/OutletService.java
@@ -1,4 +1,18 @@
 package com.bnagritech.tradesphere.outlet.service;
+import com.bnagritech.tradesphere.outlet.dto.OutletRequest;
+import com.bnagritech.tradesphere.outlet.dto.OutletResponse;
+import java.util.List;
 
 public interface OutletService {
+
+    OutletResponse createOutlet(OutletRequest request);
+    OutletResponse getOutletByOutletId(String outletId);
+    OutletResponse updateOutlet(String OutletId,OutletRequest request);
+    OutletResponse getOutletByPhoneNumber(String phoneNumber);
+    OutletResponse getOutletByEmail(String email);
+    OutletResponse updateOutletStatus(String outletId, OutletRequest request);
+    List<OutletResponse> getOutletsByTerritoryId(String territoryId);
+    List<OutletResponse> getOutletsByOutletName(String outletName);
+    List<OutletResponse> getOutletsByPromoterId(String promoterId);
+    void deleteOutlet (String outletId );
 }
