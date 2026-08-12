bf5006f35c6d8db038fc32f7427d66ea5a84a89f
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-12 12:29:34 +0530

    created Outlet.java in outlet module

---
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/outlet/model/Outlet.java
@@ -0,0 +1,48 @@
+package com.bnagritech.tradesphere.outlet.model;
+
+import com.bnagritech.tradesphere.common.enums.RetailerStatus;
+import com.bnagritech.tradesphere.common.enums.RetailerType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "outlets")
+public class Outlet {
+    @Id
+    private String id;
+    private String outletId;
+    private String outletName;
+    private String phoneNumber;
+    private String alternateContactNumber;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private String territoryId;
+    private String beatId;
+    private String promoterId;
+    private RetailerType retailerType;
+    private String gstNumber;
+    private String panNumber;
+    private Double latitude;
+    private Double longitude;
+    private Double creditDays;
+    private Double creditLimits;
+    private RetailerStatus retailerStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String createdBy;
+    private  String updatedBy;
+
+
+
+}
