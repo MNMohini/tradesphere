e695d7142183e47d417813d77402a23cd2f9df43
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-13 12:59:22 +0530

    Added abstract methods in FileDocService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/file/service/FileDocService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/file/service/FileDocService.java
@@ -1,5 +1,15 @@
 package com.bnagritech.tradesphere.file.service;
 
+import com.bnagritech.tradesphere.file.dto.FileResponse;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface FileDocService {
 
+    FileResponse uploadFile(MultipartFile file, String module, String referenceId);
+    Resource downloadFile(String fileId);
+    List<FileResponse> getFiles(String module, String referenceId);
+    void deleteFile(String fileId);
 }
