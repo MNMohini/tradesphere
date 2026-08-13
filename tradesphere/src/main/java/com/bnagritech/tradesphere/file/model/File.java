98ab6587140623f1e753f9c094ca6de838d0497c
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-13 12:21:22 +0530

    added fields in File.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/file/model/File.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/file/model/File.java
@@ -1,4 +1,30 @@
 package com.bnagritech.tradesphere.file.model;
 
+import com.bnagritech.tradesphere.common.enums.Modules;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "files")
+
 public class File {
+    @Id
+    private String id;
+    private String originalFileName;
+    private String storedFileName;
+    private Long fileSize;
+    private String storageKey;
+    private String fileUrl;
+    private Modules module;
+    private String referenceId;
+    private LocalDateTime uploadedAt;
+    private LocalDateTime updatedAt;
+
 }
