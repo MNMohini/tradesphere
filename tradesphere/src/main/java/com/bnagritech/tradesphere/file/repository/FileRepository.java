7de31d6b27fdb35a237842085d6fc4e95fcd6b57
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-13 14:40:35 +0530

    remove Download Function from here

--- tradesphere/src/main/java/com/bnagritech/tradesphere/file/repository/FileRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/file/repository/FileRepository.java
@@ -4,9 +4,10 @@ import com.bnagritech.tradesphere.file.model.FileDocuments;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FileRepository extends MongoRepository<FileDocuments,String> {
-
+    Optional<FileDocuments>findByFileId(String fileId);
     List<FileDocuments> findByModuleAndReferenceId(String module, String referenceId);
     List<FileDocuments> findByUploadedBy(String uploadedBy);
 }
