09000a12fe24f96ff947f94131284c4117475ad2
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-13 13:06:02 +0530

    implemented FileDocService

--- tradesphere/src/main/java/com/bnagritech/tradesphere/file/service/impl/FileDocServiceImpl.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/file/service/impl/FileDocServiceImpl.java
@@ -1,9 +1,34 @@
 package com.bnagritech.tradesphere.file.service.impl;
 
+import com.bnagritech.tradesphere.file.dto.FileResponse;
+import com.bnagritech.tradesphere.file.service.FileDocService;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
-public class FileDocServiceImpl {
+public class FileDocServiceImpl implements FileDocService {
 
 
+    @Override
+    public FileResponse uploadFile(MultipartFile file, String module, String referenceId) {
+        return null;
+    }
+
+    @Override
+    public Resource downloadFile(String fileId) {
+        return null;
+    }
+
+    @Override
+    public List<FileResponse> getFiles(String module, String referenceId) {
+        return List.of();
+    }
+
+    @Override
+    public void deleteFile(String fileId) {
+
+    }
 }
