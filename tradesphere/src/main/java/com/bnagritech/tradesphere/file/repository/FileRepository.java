904f8984a7a715b82ab668c921589cbfa68d563d
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-13 12:32:45 +0530

    added abstract methods in FileRepository.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/file/repository/FileRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/file/repository/FileRepository.java
@@ -3,5 +3,10 @@ package com.bnagritech.tradesphere.file.repository;
 import com.bnagritech.tradesphere.file.model.FileDocuments;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface FileRepository extends MongoRepository<FileDocuments,String> {
+
+    List<FileDocuments> findByModuleAndReferenceId(String module, String referenceId);
+    List<FileDocuments> findByUploadedBy(String uploadedBy);
 }
