359edde08b43578260a8e0380bd95d3e45f3b191
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 16:23:41 +0530

    Updated OrderResponse.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderResponse.java
@@ -3,8 +3,13 @@ package com.bnagritech.tradesphere.order.dto;
 import com.bnagritech.tradesphere.common.enums.OrderSource;
 import com.bnagritech.tradesphere.common.enums.OrderStatus;
 import com.bnagritech.tradesphere.common.enums.OrderType;
+import com.bnagritech.tradesphere.order.model.DeliveryInfo;
+import com.bnagritech.tradesphere.order.model.OrderItems;
+import com.bnagritech.tradesphere.order.model.OrderStatusHistory;
+import com.bnagritech.tradesphere.order.model.PaymentInfo;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,13 +18,38 @@ import java.util.List;
 @Data
 @Builder
 public class OrderResponse {
-
+    @Id
+    private String id;
+    private String orderNumber;
+    private LocalDateTime orderDate;
     private OrderType orderType;
     private OrderSource orderSource;
     private String outletId;
     private String promoterId;
+    private String employeeId;
     private String territoryId;
+    private String outletName;
+    private String phoneNumber;
+    private String address;
+    private List<OrderItems> items;
+    private BigDecimal grossAmount;
+    private BigDecimal itemDiscount;
+    private BigDecimal orderDiscount;
+    private BigDecimal taxableAmount;
+    private BigDecimal taxAmount;
+    private BigDecimal roundOff;
+    private BigDecimal netAmount;
+    private OrderStatus orderStatus;
+    private List<OrderStatusHistory> statusHistory;
+    private PaymentInfo paymentInfo;
+    private DeliveryInfo deliveryInfo;
+    private String cancellationReason;
+    private String cancelledBy;
+    private LocalDateTime cancelledAt;
     private String remarks;
-    private List<OrderItemRequest> items;
+    private String createdBy;
+    private String updatedBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 }
