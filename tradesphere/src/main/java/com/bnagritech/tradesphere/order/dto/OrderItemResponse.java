47c2a364f7f48820b1e649b2eaa1c9f1718efca5
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 16:06:41 +0530

    updated OrderItemResponse. java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderItemResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderItemResponse.java
@@ -9,8 +9,8 @@ import java.math.BigDecimal;
 public class OrderItemResponse {
     private String productId;
     private String productName;
-
+    private BigDecimal mrp;
     private Integer quantity;
-    private BigDecimal unitPrice;
+    private BigDecimal sellingPrice;
     private BigDecimal totalPrice;
 }
