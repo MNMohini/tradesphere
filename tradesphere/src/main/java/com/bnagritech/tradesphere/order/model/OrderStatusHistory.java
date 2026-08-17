a8c1154ad76d4c79dae007f50aff41175f1d6700
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 14:47:47 +0530

    added fields in OrderStatusHistory.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/OrderStatusHistory.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/OrderStatusHistory.java
@@ -1,4 +1,13 @@
 package com.bnagritech.tradesphere.order.model;
 
+import com.bnagritech.tradesphere.common.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+
 public class OrderStatusHistory {
+    private OrderStatus orderStatus;
+    private String changedBy;
+    private String remark;
+    private LocalDateTime changeAt;
+
 }
