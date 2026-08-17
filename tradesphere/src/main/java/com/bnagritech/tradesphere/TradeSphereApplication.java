75ebe0ecd7b99fe7d7b743d9a6152be840d48ad7
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 15:03:42 +0530

    Added mongoDb Annotation

--- tradesphere/src/main/java/com/bnagritech/tradesphere/TradeSphereApplication.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/TradeSphereApplication.java
@@ -2,8 +2,10 @@ package com.bnagritech.tradesphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class TradeSphereApplication {
 
     public static void main(String[] args) {
