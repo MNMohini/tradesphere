93a1ac890102f1951d92e9c798cc791d58cc3607
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 15:51:09 +0530

    updated OrdeItemRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderItemRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderItemRequest.java
@@ -3,7 +3,12 @@ package com.bnagritech.tradesphere.order.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 public class OrderItemRequest {
+    private String skuCode;
+    private BigDecimal sellingPrice;
+    private Integer quantity;
 }
