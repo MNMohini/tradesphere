d3750bf5ee342ffbe7c90bd906adbcb3c7891264
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 14:54:22 +0530

    added fields in PaymentInfo.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/PaymentInfo.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/PaymentInfo.java
@@ -1,4 +1,18 @@
 package com.bnagritech.tradesphere.order.model;
 
+import com.bnagritech.tradesphere.common.enums.PaymentMode;
+import com.bnagritech.tradesphere.common.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentInfo {
+    private PaymentStatus paymentStatus;
+    private PaymentMode paymentMode;
+    private BigDecimal totalAmount;
+    private BigDecimal paidAmount;
+    private BigDecimal pendingAmount;
+    private String transactionReference;
+    private LocalDateTime finalPaymentDate;
+
 }
