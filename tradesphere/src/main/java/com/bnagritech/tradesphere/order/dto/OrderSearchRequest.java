5b93eadc9f982c1eec4307db9eb129139e36210e
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 16:50:16 +0530

    added fields in OrderSearchRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderSearchRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderSearchRequest.java
@@ -1,4 +1,21 @@
 package com.bnagritech.tradesphere.order.dto;
 
+import com.bnagritech.tradesphere.common.enums.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderSearchRequest {
+    private String orderNumber;
+    private String outletId;
+    private String promoterId;
+    private String territoryId;
+    private OrderStatus orderStatus;
+    private LocalDate fromDate;
+     private LocalDate toDate;
 }
