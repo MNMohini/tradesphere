12cf1b17ed8d6301e5402971140d2b031e1e55df
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 16:14:57 +0530

    created DeliveryInfoResponse.java and added some fields in it

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/DeliveryInfoResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/DeliveryInfoResponse.java
@@ -1,4 +1,16 @@
 package com.bnagritech.tradesphere.order.dto;
+import com.bnagritech.tradesphere.common.enums.DeliveryStatus;
+import lombok.Builder;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class DeliveryInfoResponse {
+    private String deliveryAddress;
+    private DeliveryStatus deliveryStatus;
+    private String deliveredBy;
+    private LocalDateTime deliveredAt;
+    private String deliveryRemark;
 }
