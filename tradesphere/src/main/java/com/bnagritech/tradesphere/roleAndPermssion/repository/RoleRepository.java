4f60c1d1dd995c39bd6547248bc4e89111f06db9
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 13:13:32 +0530

    remove role and permission folder

--- tradesphere/src/main/java/com/bnagritech/tradesphere/roleAndPermssion/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bnagritech.tradesphere.roleAndPermssion.repository;
-import com.bnagritech.tradesphere.roleAndPermssion.model.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-
-    boolean existsByEmployeeId(String employeeId);
-}
