3a89a5cba2f61db75e18ab63df9380fb5817eafe
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 16:40:58 +0530

    added fields in OrderSummaryResponse.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderSummaryResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderSummaryResponse.java
@@ -1,4 +1,21 @@
 package com.bnagritech.tradesphere.order.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderSummaryResponse {
+    private long totalOrders;
+    private long confirmedOrder;
+    private long createdOrders;
+    private long processingOrders;
+    private long dispatchedOrders;
+    private long deliveredOrders;
+    private long cancelledOrders;
+    private BigDecimal totalSales;
+
 }
