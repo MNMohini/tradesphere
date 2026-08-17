05419f86a77d8c65d03e0b1db2020ecc252b64e7
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 15:00:13 +0530

    added fields in DeliveryInfo.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/DeliveryInfo.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/model/DeliveryInfo.java
@@ -1,5 +1,17 @@
 package com.bnagritech.tradesphere.order.model;
 
+import com.bnagritech.tradesphere.common.enums.DeliveryStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class DeliveryInfo {
-    
+    private String deliveryAddress;
+    private DeliveryStatus deliveryStatus;
+    private LocalDate expectedDeliveryDate;
+    private LocalDate actualDeliveryDate;
+    private String deliveredBy;
+    private LocalDateTime deliveredAt;
+    private String deliveryRemark;
+
 }
