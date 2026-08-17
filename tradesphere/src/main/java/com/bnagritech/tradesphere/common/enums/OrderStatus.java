a0b83bb5eacb36d6ab722f09f26283e90ac6b3cd
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-17 15:03:13 +0530

    updated OrderStatus.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/OrderStatus.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/common/enums/OrderStatus.java
@@ -1,12 +1,10 @@
 package com.bnagritech.tradesphere.common.enums;
 
 public enum OrderStatus {
-    DRAFT,
     PLACED,
     PENDING_APPROVAL,
     APPROVED,
     CONFIRMED,
-    PARTIALLY_FULFILLED,
     DISPATCHED,
     DELIVERED,
     REJECTED,
