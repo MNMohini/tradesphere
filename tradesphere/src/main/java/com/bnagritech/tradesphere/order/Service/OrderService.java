6014e65fcbbabd93050d9ec7d559338616fbb411
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-18 16:32:28 +0530

    updated OrderService.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/Service/OrderService.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/Service/OrderService.java
@@ -1,5 +1,23 @@
 package com.bnagritech.tradesphere.order.Service;
 
+import com.bnagritech.tradesphere.order.dto.*;
+
+import java.util.List;
+
 public interface OrderService {
 
+    OrderResponse createOrder(OrderRequest request);
+    List<OrderResponse> getAllOrders();
+    OrderResponse getOrderById(String id);
+    OrderResponse getOrderByOrderNumber(String orderNumber);
+    List<OrderResponse> getOrdersByOutletId(String outletId);
+    List<OrderResponse> getOrdersByPromoterId(String promoterId);
+    List<OrderResponse> getOrdersByEmployeeId(String employeeId);
+    List<OrderResponse> getOrdersByTerritoryId(String territoryId);
+    List<OrderResponse> getOrdersByStatus(String status);
+    OrderResponse updateOrderStatus(String orderId, UpdatedOrderStatusRequest request);
+    OrderResponse updateOrder(String id, OrderRequest request);
+    void deleteOrder(String id);
+    List<OrderResponse> searchOrders(OrderSearchRequest request);
+    OrderSummaryResponse getOrderSummary();
 }
