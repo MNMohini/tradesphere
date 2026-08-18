d1bef9ff5c041918063cc98db96c83e9f0c92408
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-18 16:20:52 +0530

    update DeliveryInfoResponse.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/DeliveryInfoResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/DeliveryInfoResponse.java
@@ -1,11 +1,15 @@
 package com.bnagritech.tradesphere.order.dto;
 import com.bnagritech.tradesphere.common.enums.DeliveryStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class DeliveryInfoResponse {
     private String deliveryAddress;
