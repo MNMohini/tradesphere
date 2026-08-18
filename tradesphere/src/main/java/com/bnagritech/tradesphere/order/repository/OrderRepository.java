5ea9c10c1275d4920c5a5d8fab131df333a5d8e9
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-18 16:22:30 +0530

    OrderRepository.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
@@ -1,8 +1,26 @@
 package com.bnagritech.tradesphere.order.repository;
 
+import com.bnagritech.tradesphere.common.enums.OrderStatus;
 import com.bnagritech.tradesphere.order.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+@Repository
 public interface OrderRepository extends MongoRepository<Order,String> {
 
+    Optional<Order> findByOrderNumber(String orderNumber);
+    boolean existsByOrderNumber(String orderNumber);
+    List<Order> findByOutletId(String outletId);
+    List<Order> findByPromoterId(String promoterId);
+    List<Order> findByTerritoryId(String territoryId);
+    List<Order> findByOrderStatus(OrderStatus orderStatus);
+    List<Order> findByOutletIdAndOrderStatus(String outletId, OrderStatus orderStatus);
+    List<Order> findByPromoterIdAndOrderStatus(String promoterId, OrderStatus orderStatus);
+    List<Order> findByOrderDateBetween(LocalDate fromDate, LocalDate toDate);
+    List<Order> findByOutletIdAndOrderDateBetween(String retailerId, LocalDate fromDate, LocalDate toDate);
+    List<Order> findByPromoterIdAndOrderDateBetween(String promoterId, LocalDate fromDate, LocalDate toDate);
+    List<Order> findByTerritoryIdAndOrderStatus(String territoryId, OrderStatus orderStatus);
 }
