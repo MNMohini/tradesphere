e76e1120640643c7f91b5622cdcdb2ffa74d0fca
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-18 16:23:08 +0530

    Update OrderRequest.java and OrderResponse.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderResponse.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/OrderResponse.java
@@ -1,37 +1,31 @@
 package com.bnagritech.tradesphere.order.dto;
 
 import com.bnagritech.tradesphere.common.enums.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class OrderResponse {
     private String id;
     private String orderNumber;
-
-    private String retailerId;
-    private String retailerName;
-
-    private String distributorId;
-    private String distributorName;
-
+    private String outletId;
+    private String outletName;
     private String promoterId;
     private String promoterName;
-
     private List<OrderItemResponse> items;
-
     private Integer totalQuantity;
     private BigDecimal totalAmount;
-
     private OrderStatus status;
-
     private String remarks;
-
     private LocalDateTime orderDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
