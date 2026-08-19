0425316953da2ef2eacb8c7ca62a495ac7e255ee
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-19 12:22:08 +0530

    updated OrderRepository.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/repository/OrderRepository.java
@@ -5,22 +5,27 @@ import com.bnagritech.tradesphere.order.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends MongoRepository<Order,String> {
 
     Optional<Order> findByOrderNumber(String orderNumber);
     boolean existsByOrderNumber(String orderNumber);
     List<Order> findByOutletId(String outletId);
     List<Order> findByPromoterId(String promoterId);
+    List<Order> findByEmployeeId(String employeeId);
     List<Order> findByTerritoryId(String territoryId);
-    List<Order> findByOrderStatus(OrderStatus orderStatus);
-    List<Order> findByOutletIdAndOrderStatus(String outletId, OrderStatus orderStatus);
-    List<Order> findByPromoterIdAndOrderStatus(String promoterId, OrderStatus orderStatus);
-    List<Order> findByOrderDateBetween(LocalDate fromDate, LocalDate toDate);
-    List<Order> findByOutletIdAndOrderDateBetween(String retailerId, LocalDate fromDate, LocalDate toDate);
-    List<Order> findByPromoterIdAndOrderDateBetween(String promoterId, LocalDate fromDate, LocalDate toDate);
-    List<Order> findByTerritoryIdAndOrderStatus(String territoryId, OrderStatus orderStatus);
+    List<Order> findByStatus(OrderStatus status);
+    List<Order> findByOrderDateBetween(LocalDateTime fromDate, LocalDateTime toDate);
+    List<Order> findByOutletIdAndStatus(String outletId, OrderStatus status);
+    List<Order> findByPromoterIdAndStatus(String promoterId, OrderStatus status);
+    List<Order> findByEmployeeIdAndStatus(String employeeId, OrderStatus status);
+    List<Order> findByTerritoryIdAndStatus(String territoryId, OrderStatus status);
+    List<Order> findByOutletIdAndOrderDateBetween(String outletId, LocalDateTime fromDate, LocalDateTime toDate);
+    List<Order> findByPromoterIdAndOrderDateBetween(String promoterId, LocalDateTime fromDate, LocalDateTime toDate);
+    List<Order> findByEmployeeIdAndOrderDateBetween(String employeeId, LocalDateTime fromDate, LocalDateTime toDate);
+    List<Order> findByTerritoryIdAndOrderDateBetween(String territoryId, LocalDateTime fromDate, LocalDateTime toDate);
 }
