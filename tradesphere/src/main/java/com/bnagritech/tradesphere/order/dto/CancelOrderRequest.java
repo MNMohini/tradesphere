72f250d94bcd5109991c2a0035bcf333524af2ee
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-19 12:14:52 +0530

    added fields in CancelOrderRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/CancelOrderRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/order/dto/CancelOrderRequest.java
@@ -1,4 +1,12 @@
 package com.bnagritech.tradesphere.order.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CancelOrderRequest {
+    private String remark;
 }
