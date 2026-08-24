a59f34da89b47310c9ec3c51b5b03aa2af5c8d73
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-24 13:34:52 +0530

    update TerritoryRequest.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/territory/dto/TerritoryRequest.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/territory/dto/TerritoryRequest.java
@@ -1,15 +1,18 @@
 package com.bnagritech.tradesphere.territory.dto;
 
+import com.bnagritech.tradesphere.beat.model.Beat;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class TerritoryRequest {
-
+    @NotBlank(message= "required fields")
     private String territoryId;
+    @NotBlank(message= "required fields")
     private String territoryName;
-    private String state;
-    private String city;
-    private String description;
-    private Boolean active;
-    private String territoryType;
+    private List<Beat> beats;
+    private List<String> state;
+    private List<String> city;
+
 }
