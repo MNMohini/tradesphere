26a23779bf82c845269a15ee5533edf9f72bd6bb
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-27 11:42:57 +0530

    added annotation and added some required fields in SalesRepresentative.java

--- tradesphere/src/main/java/com/bnagritech/tradesphere/salesrepresentativemodule/model/SalesRepresentative.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/salesrepresentativemodule/model/SalesRepresentative.java
@@ -1,4 +1,39 @@
 package com.bnagritech.tradesphere.salesrepresentativemodule.model;
 
+import com.bnagritech.tradesphere.common.enums.UserRole;
+import com.bnagritech.tradesphere.common.enums.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection= "SR")
 public class SalesRepresentative {
+    @Id
+    private String id;
+    private String SRId;
+    private String SRName;
+    private String userName;
+    private String phoneNumber;
+    private String email;
+    private String territoryId;
+    private String city;
+    private String state;
+    private UserStatus status;
+    private UserRole role;
+    private LocalDateTime CreatedAt;
+    private LocalDateTime UpdatedAt;
+
+
+
+
+
 }
