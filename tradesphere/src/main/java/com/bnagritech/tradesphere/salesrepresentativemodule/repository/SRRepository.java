e46b091addcbece253596b553e074585f60c3055
Mohini <mohini.chaturvedi@bngroupindia.com>
2026-08-27 12:09:12 +0530

    added some required function in SRRepository.java interface

--- tradesphere/src/main/java/com/bnagritech/tradesphere/salesrepresentativemodule/repository/SRRepository.java
+++ tradesphere/src/main/java/com/bnagritech/tradesphere/salesrepresentativemodule/repository/SRRepository.java
@@ -1,4 +1,23 @@
 package com.bnagritech.tradesphere.salesrepresentativemodule.repository;
 
-public interface SRRepository {
+import com.bnagritech.tradesphere.common.enums.UserStatus;
+import com.bnagritech.tradesphere.salesrepresentativemodule.model.SalesRepresentative;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SRRepository extends MongoRepository<SalesRepresentative, String> {
+    Optional<SalesRepresentative> findBySRId(String SRId);
+    Optional<SalesRepresentative> findByEmail(String email);
+    Optional<SalesRepresentative> findByPhoneNumber(String phoneNumber);
+    List<SalesRepresentative> findByTerritoryId(String  territoryId);
+    List<SalesRepresentative> findByStatus(UserStatus status);
+    boolean existsBySRId(String SRId);
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+
+
 }
